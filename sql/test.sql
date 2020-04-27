5b17e0aaf758868b74f143223b40d069cbf03bdf
Remulef <fabienremule974@gmail.com>
2020-04-27 16:56:42 +0200

    test.sql

--- sql/test.sql
+++ sql/test.sql
@@ -1,8 +1,8 @@
 SELECT *
 FROM DOCUMENT
-WHERE id_doc IN ( SELECT DOCUMENT_id_doc
-				  FROM NODE_DOCUMENT
-				  WHERE DOCUMENT_id_node IN (SELECT id_node
-                                             FROM NODE
-                                             WHERE name =" Administration")
+WHERE DOCUMENT.id_doc in ( SELECT DOCUMENT_id_doc
+                           FROM NODE_DOCUMENT
+                           WHERE DOCUMENT_id_node in (SELECT id_node
+                                                      FROM NODE
+                                                      WHERE name =" Administration")
 );
