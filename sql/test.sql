df47d65ae142e9d7b1cbb76558dcf5f6bab53597
Remulef <fabienremule974@gmail.com>
2020-04-30 19:26:19 +0200

    l

--- sql/test.sql
+++ sql/test.sql
@@ -1,9 +1,11 @@
 SELECT *
 FROM DOCUMENT
-WHERE DOCUMENT.id_doc in ( SELECT DOCUMENT_id_doc
+WHERE typedoc="pdf"and  id_doc in ( SELECT DOCUMENT_id_doc
                            FROM NODE_DOCUMENT
                            WHERE NODE_id_node in (SELECT id_node
                                                       FROM NODE
-                                                      WHERE parent_node_id in(236,145,984))
+                                                      WHERE name="E" and parent_node_id in (select id_node 
+																					 from NODE
+                                                                                     where name="Administrations"))
 );
 
