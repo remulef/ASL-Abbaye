eef53e9bd3098211faee0ccc20d2dde72a388bc0
remulef <50622976+remulef@users.noreply.github.com>
2020-06-05 01:32:38 +0200

    automatic push

--- Controler/script-document.php
+++ Controler/script-document.php
@@ -3,48 +3,37 @@
 
 
 //if(isset( $_POST['id_doc'])){
-if(true){
+if (true) {
 
-    //$id_doc = $_POST['data'];
-    $id_doc = 238;
-    $id_doc = random_int(2,1000);
-    echo $id_doc.PHP_EOL;
+  //$id_doc = $_POST['data'];
+  $id_doc = 238;
+  $id_doc = random_int(2, 1000);
+  echo $id_doc . PHP_EOL;
 
-    //il faudra checker l'identhitifcation
+  //il faudra checker l'identhitifcation
 
 
 
-    //On ouvre la base de donnée
-    $database = 'gsjrnmiasl.mysql.db';
-    $user = 'gsjrnmiasl';
-    $password = 'MJCAbbaye38';
-    try {
-        $db = new PDO("mysql:host=gsjrnmiasl.mysql.db;dbname=gsjrnmiasl", $user,$password);
+  //On ouvre la base de donnée
+  $database = 'gsjrnmiasl.mysql.db';
+  $user = 'gsjrnmiasl';
+  $password = 'MJCAbbaye38';
+  try {
+    $db = new PDO("mysql:host=gsjrnmiasl.mysql.db;dbname=gsjrnmiasl", $user, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //echo "Connected successfully";
-  }
-  catch (Exception $e)
-  {
+  } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
   }
 
 
-  $sth= $db->prepare('Select * FROM `DOCUMENT` WHERE id_doc = ?');
-  $sth->bindParam(1,$id_doc);
+  $sth = $db->prepare('Select * FROM `DOCUMENT` WHERE id_doc = ?');
+  $sth->bindParam(1, $id_doc);
 
   $sth->execute();
-  $res = $sth->fetch(FETCH_ASSOC);
-
-
-
-var_dump($res);
-$json = json_encode($doc);
-echo $json;
-$db=null;
-
-
-
+  $res = $sth->fetch(PDO::FETCH_ASSOC);
+  var_dump($res);
+  $json = json_encode($doc);
+  echo $json;
+  $db = null;
 }
-
-
-?>
