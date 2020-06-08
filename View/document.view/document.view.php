940a8a0223c05355a23bb0aa5a113f6542605ca6
remulef <50622976+remulef@users.noreply.github.com>
2020-06-08 12:27:04 +0200

    automatic push

--- View/document.view/document.view.php
+++ View/document.view/document.view.php
@@ -70,6 +70,7 @@ if (empty($_GET['id_doc'])) {
                         <span class="tooltiptext">Supprimer le document</span>
                     </button>
                 </div>
+                <div class="tooltip">
                     <button type="button" id="modifier" onclick="modifier()">
                         <svg class="bi bi-pencil-square" width="1.33em" height="1.33em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path d="M15.502 1.94a.5.5 0 010 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 01.707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 00-.121.196l-.805 2.414a.25.25 0 00.316.316l2.414-.805a.5.5 0 00.196-.12l6.813-6.814z" />
