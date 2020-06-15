c1afa4c05d48081699d3e9f7c7c70250613add70
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-06-15 23:34:02 +0200

    automatic push

--- Controler/Display/script-load.php
+++ Controler/Display/script-load.php
@@ -16,9 +16,8 @@ try {
 
 $data = $_POST['data'];
 //$data = json_decode($data);
-$data = 12;
 //Si data est vide alors on initialise à la racine
-if (!isset($data)) {
+if (isset($data)) {
     $node_name = "Ressourcepeda";
     $sth = $db->prepare('SELECT * 
     FROM DOCUMENT 
@@ -49,7 +48,7 @@ if (!isset($data)) {
     foreach ($node as $key => $value) {
         $node[$key]["name"] = utf8_encode($value["name"]);
     }
-
+    
     foreach ($document as $key => $value) {
         $document[$key]["nom"] = utf8_encode($value["nom"]);
         $document[$key]["chemin"] = utf8_encode($value["chemin"]);
@@ -59,7 +58,11 @@ if (!isset($data)) {
     $array = array_merge($node, $document);
     $json = json_encode($array);
     echo $json;
-}else if(isset($data)) {
+}
+//Si data n'est pas vide 
+//alors il s'agit des parametres de tri 
+//Ici on charge un dossier spécifique avec son id
+else {
     //$id_node = $data->id_node;
     $id_node = $data;
     $sth = $db->prepare('SELECT * 
@@ -72,6 +75,8 @@ if (!isset($data)) {
     $sth->execute();
     $document = $sth->fetchAll(PDO::FETCH_ASSOC);
     var_dump($document);
+    echo PHP_EOL;
+
     $sth = $db->prepare(
         'SELECT * 
     FROM NODE 
@@ -81,13 +86,12 @@ if (!isset($data)) {
     $sth->bindParam(1, $id_node);
     $sth->execute();
     $node = $sth->fetchAll(PDO::FETCH_ASSOC);
-    var_dump($node);
-
-
+    
+    
     foreach ($node as $key => $value) {
         $node[$key]["name"] = utf8_encode($value["name"]);
     }
-
+    
     foreach ($document as $key => $value) {
         $document[$key]["nom"] = utf8_encode($value["nom"]);
         $document[$key]["chemin"] = utf8_encode($value["chemin"]);
