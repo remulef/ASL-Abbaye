2ec9387352a7a5242d33d5b9f206ce26663075c8
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-06-15 19:43:11 +0200

    automatic push

--- Controler/Display/script-load.php
+++ Controler/Display/script-load.php
@@ -14,17 +14,10 @@ try {
 
 $data = $_POST['data'];
 $data = json_decode($data);
+
+//Si data est vide alors on initialise à la racine
 if (empty($data)) {
-    $node_name = "%RessourcePeda%";
-}
-else {
-    $node_name = $data->node;
-}
-
-echo PHP_EOL;    
-
-
-
+    $node_name = "Ressourcepeda";
     $sth = $db->prepare('SELECT * 
     FROM DOCUMENT 
     WHERE id_doc IN (SELECT DOCUMENT_id_doc 
@@ -32,7 +25,7 @@ echo PHP_EOL;
                      WHERE NODE_id_node IN 
                                      (SELECT id_node 
                                      FROM NODE 
-                                     WHERE name LIKE ? ))');
+                                     WHERE name = ? ))');
 
     $sth->bindParam(1, $node_name);
     $sth->execute();
@@ -45,7 +38,7 @@ echo PHP_EOL;
     FROM NODE 
     WHERE parent_node_id IN (SELECT id_node 
                             FROM NODE 
-                            WHERE name LIKE ? )');
+                            WHERE name = ? )');
 
     $sth->bindParam(1, $node_name);
     $sth->execute();
@@ -55,8 +48,36 @@ echo PHP_EOL;
     $json = array_merge($node,$document);
     $json = json_encode($json);
     echo $json;
-    
-    
-    
-    
+}
+//Si data n'est pas vide 
+//alors il s'agit des parametres de tri 
+//Ici on charge un dossier spécifique avec son id
+else {
+    $id_node = $data->id_node;
+    $sth = $db->prepare('SELECT * 
+    FROM DOCUMENT 
+    WHERE id_doc IN (SELECT DOCUMENT_id_doc 
+                     FROM NODE_DOCUMENT 
+                     WHERE NODE_id_node = ?');
+
+    $sth->bindParam(1,$id_node);
+    $sth->execute();
+    $document = $sth->fetchAll(PDO::FETCH_ASSOC);
+    var_dump($document);
+    echo PHP_EOL;    
+
+    $sth = $db->prepare(
+    'SELECT * 
+    FROM NODE 
+    WHERE parent_node_id = ? ');
+
+    $sth->bindParam(1,$id_node);
+    $sth->execute();
+    $node = $sth->fetchAll(PDO::FETCH_ASSOC);
+    var_dump($node);
+
+    $json = array_merge($node,$document);
+    $json = json_encode($json);
+    echo $json;
+}
 $db = null;
