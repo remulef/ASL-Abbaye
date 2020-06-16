5faeafc7e80af0473557edb1c35c54cdfbccc45f
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-06-16 22:38:06 +0200

    automatic push

--- View/display.view/display.js
+++ View/display.view/display.js
@@ -32,7 +32,7 @@ function load(json) {
             var id = current.id_node;
             var li = document.createElement("li");
             var a = document.createElement("a");
-            a.setAttribute("onclick", "changedoc(" + id + ',"' + current.name + '",true,false)');
+            a.setAttribute("onclick", "changedoc(" + id + ',"' + current.name + '",true,-1)');
             a.setAttribute("class", "button3");
             a.innerHTML = current.name;
             li.appendChild(a);
