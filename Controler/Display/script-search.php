f794b2b8eefc779492b7d96c1791888e46bff148
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-06-18 00:51:07 +0200

    automatic push

--- Controler/Display/script-search.php
+++ Controler/Display/script-search.php
@@ -19,14 +19,14 @@ $data = json_decode($data);
 //$tefanf = $data->tefanf; 
 
 
-($data->format == "" ? $search_format = "" : $search_format = " AND typedoc like \"%" . $data->format . "%\"");
-($data->docname == "" ? $search_name = "" : $search_name = " AND nom like \"%" . $data->docname . "%\"");
-((count($data->typedoc) > 0) ? $typedoc =  " AND typedoc in (\"" . implode("\",\"", $data->typedoc) . "\")" : $typedoc = "");
-((count($data->niveau) > 0) ? $niveau = " AND nom like \"%" . implode("%", $data->niveau) . "%\"" : $niveau = "");
-$order = " ORDER BY nom " . ($data->order == true ? "ASC" : "DESC");
-$node = " AND id_doc IN (SELECT DOCUMENT_id_doc FROM NODE_DOCUMENT WHERE NODE_id_node = " . $data->pos . ")";
+($data->format ==""?$search_format="":$search_format=" AND typedoc like \"%".$data->format."%\"");
+($data->docname ==""?$search_name="":$search_name=" AND nom like \"%".$data->docname."%\"");
+((count($data->typedoc)>0)?$typedoc =  " AND typedoc in (\"".implode("\",\"",$data->typedoc)."\")":$typedoc="");
+((count($data->niveau)>0)?$niveau = " AND nom like \"%".implode("%",$data->niveau)."%\"":$niveau="");
+$order =" ORDER BY nom ".($data->order==true?"ASC":"DESC");
+$node = " AND id_doc IN (SELECT DOCUMENT_id_doc FROM NODE_DOCUMENT WHERE NODE_id_node = ".$data->pos.")";
 $query = 'SELECT * FROM DOCUMENT WHERE 1 ';
-$query = $query . $search_format . $search_name . $typedoc . $niveau . $order; //.$node
+$query = $query.$search_format.$search_name.$typedoc.$niveau.$order; //.$node
 
 echo $query;
 $sth = $db->query($query);
