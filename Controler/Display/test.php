35b99e86c0026a5fafc306c418eeb4079967da7d
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-06-17 22:36:28 +0200

    automatic push

--- Controler/Display/test.php
+++ Controler/Display/test.php
@@ -6,12 +6,12 @@ $data = json_decode($data);
 
 ($data->format ==""?$search_format="":$search_format=" AND typedoc like %".$data->format."%");
 ($data->name ==""?$search_name="":$search_name=" AND nom like %".$data->name."%");
-((count($data->typedoc)>0)?$typedoc =  " AND typedoc in (\"".implode(" \",\"",$$data->typedoc)."\")":$typedoc="");
+((count($data->typedoc)>0)?$typedoc =  " AND typedoc in (\"".implode(" \",\"",$data->typedoc)."\")":$typedoc="");
 ((count($data->niveau)>0)?$niveau = " AND nom like %".implode("%",$data->niveau)."%":$niveau="");
 $order =" ORDER BY nom "($data->order==true?"ASC":"DESC");
 $node = " AND id_doc IN (SELECT DOCUMENT_id_doc FROM NODE_DOCUMENT WHERE NODE_id_node = ".$data->pos.")";
 $query = 'SELECT * FROM DOCUMENT WHERE 1 ';
-$query = $query.$search_format.$search_name.$typedoc.$niveau.$node.$order;
+$query = $query.$search_format; //.$search_name.$typedoc.$niveau.$node.$order;
 
 echo $query;
 
