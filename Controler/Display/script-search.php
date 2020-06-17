9ef3d4a80f19f046ce085ca65205236be85612ad
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-06-17 22:34:36 +0200

    automatic push

--- Controler/Display/script-search.php
+++ Controler/Display/script-search.php
@@ -16,24 +16,20 @@ try {
 
 $data = json_decode($data);
 
-$id_node = $data->pos;
-$search_name = "%".$data->docname."%";
-$typedoc = $data->typedoc;
-$search_format = $data->formats;
-$niveau = $data->niveau;
-$order =($data->order==true?"ASC":"DESC");
-$tefanf = $data->tefanf; 
+//$tefanf = $data->tefanf; 
 
-$typedoc = "(".implode(",",$typedoc).")";
-
-
+($data->format ==""?$search_format="":$search_format=" AND typedoc like %".$data->format."%");
+($data->name ==""?$search_name="":$search_name=" AND nom like %".$data->name."%");
+((count($data->typedoc)>0)?$typedoc =  " AND typedoc in (\"".implode(" \",\"",$$data->typedoc)."\")":$typedoc="");
+((count($data->niveau)>0)?$niveau = " AND nom like %".implode("%",$data->niveau)."%":$niveau="");
+$order =" ORDER BY nom "($data->order==true?"ASC":"DESC");
+$node = " AND id_doc IN (SELECT DOCUMENT_id_doc FROM NODE_DOCUMENT WHERE NODE_id_node = ".$data->pos.")";
+$query = 'SELECT * FROM DOCUMENT WHERE 1 ';
+$query = $query.$search_format.$search_name.$typedoc.$niveau.$node.$order;
 
 $string = $_POST['data'];
 $string = "%".$string."%";
-$sth = $db->prepare('SELECT * 
-    FROM DOCUMENT 
-    WHERE nom LIKE ? 
-    ORDER BY nom ?');
+$sth = $db->prepare('');
 
 $sth->bindParam(1, $search_name);
 $sth->execute();
--- Controler/Display/test.php
+++ Controler/Display/test.php
@@ -3,15 +3,16 @@ header('Access-Control-Allow-Origin: *');
 $data = $_POST['data'];
 $data = json_decode($data);
 
-$id_node = $data->pos;
-$search_name = "%".$data->docname."%";
-$typedoc = $data->typedoc;
-$search_format = $data->formats;
-$niveau = $data->niveau;
-$order =($data->order==true?"ASC":"DESC");
-$tefanf = $data->tefanf; 
-var_dump($typedoc);
-$typedoc_string = "(\"".implode(" \",\"",$typedoc)."\")";
-echo $typedoc_string;
+
+($data->format ==""?$search_format="":$search_format=" AND typedoc like %".$data->format."%");
+($data->name ==""?$search_name="":$search_name=" AND nom like %".$data->name."%");
+((count($data->typedoc)>0)?$typedoc =  " AND typedoc in (\"".implode(" \",\"",$$data->typedoc)."\")":$typedoc="");
+((count($data->niveau)>0)?$niveau = " AND nom like %".implode("%",$data->niveau)."%":$niveau="");
+$order =" ORDER BY nom "($data->order==true?"ASC":"DESC");
+$node = " AND id_doc IN (SELECT DOCUMENT_id_doc FROM NODE_DOCUMENT WHERE NODE_id_node = ".$data->pos.")";
+$query = 'SELECT * FROM DOCUMENT WHERE 1 ';
+$query = $query.$search_format.$search_name.$typedoc.$niveau.$node.$order;
+
+echo $query;
 
 ?>
