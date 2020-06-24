3a10b236bdb95be02b177ec6a9b8d1f09a44d865
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-06-25 01:38:02 +0200

    automatic push

--- View/display.view/display.php
+++ View/display.view/display.php
@@ -22,6 +22,13 @@
                 <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 0-.5.5v6.5A2.5 2.5 0 0 0 8 13h5.5a.5.5 0 0 0 0-1H8a1.5 1.5 0 0 1-1.5-1.5V4a.5.5 0 0 0-.5-.5z" />
             </svg>
         </a>
+        <a id="hider" onclick="hide()" class="button1">
+            <svg class="bi bi-arrow-bar-left" width="3em" height="3em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M5.854 4.646a.5.5 0 0 0-.708 0l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L3.207 8l2.647-2.646a.5.5 0 0 0 0-.708z" />
+                <path fill-rule="evenodd" d="M10 8a.5.5 0 0 0-.5-.5H3a.5.5 0 0 0 0 1h6.5A.5.5 0 0 0 10 8zm2.5 6a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 1 0v11a.5.5 0 0 1-.5.5z" />
+            </svg>
+        </a>
+
         <br>
         <ul id="parcours">
         </ul>
@@ -36,13 +43,7 @@
 
 
 <body onload="init()">
-        <a id="hider" onclick="hide()">
-            <svg class="bi bi-arrow-bar-left" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M5.854 4.646a.5.5 0 0 0-.708 0l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L3.207 8l2.647-2.646a.5.5 0 0 0 0-.708z" />
-                <path fill-rule="evenodd" d="M10 8a.5.5 0 0 0-.5-.5H3a.5.5 0 0 0 0 1h6.5A.5.5 0 0 0 10 8zm2.5 6a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 1 0v11a.5.5 0 0 1-.5.5z" />
-            </svg>
-        </a>
-
+        
     <div id="sortpanel" style="display: block;">
         <form>
             <label class="description" for="element_1">Rechercher par nom </label>
