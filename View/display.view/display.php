14d97c5f81d9506747c112600fc414715c0265e7
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-06-25 01:47:18 +0200

    automatic push

--- View/display.view/display.php
+++ View/display.view/display.php
@@ -22,12 +22,6 @@
                 <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 0-.5.5v6.5A2.5 2.5 0 0 0 8 13h5.5a.5.5 0 0 0 0-1H8a1.5 1.5 0 0 1-1.5-1.5V4a.5.5 0 0 0-.5-.5z" />
             </svg>
         </a>
-        <a id="hider" onclick="hide()" class="button1">
-            <svg class="bi bi-arrow-bar-left" width="1.33em" height="1.33em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M5.854 4.646a.5.5 0 0 0-.708 0l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L3.207 8l2.647-2.646a.5.5 0 0 0 0-.708z" />
-                <path fill-rule="evenodd" d="M10 8a.5.5 0 0 0-.5-.5H3a.5.5 0 0 0 0 1h6.5A.5.5 0 0 0 10 8zm2.5 6a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 1 0v11a.5.5 0 0 1-.5.5z" />
-            </svg>
-        </a>
 
         <br>
         <ul id="parcours">
@@ -43,95 +37,104 @@
 
 
 <body onload="init()">
-        
-    <div id="sortpanel" style="display: block;">
-        <form>
-            <label class="description" for="element_1">Rechercher par nom </label>
-            <div style="float: left;">
-                <input onblur="search(this)" id="element_1" name="docname" class="element text large" type="search" maxlength="255" value="" placeholder="Recherche par nom" />
-                <hr>
-            </div>
+    <div>
+        <a id="hider" onclick="hide()" class="button1">
+            <svg class="bi bi-arrow-bar-left" width="1.33em" height="1.33em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M5.854 4.646a.5.5 0 0 0-.708 0l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L3.207 8l2.647-2.646a.5.5 0 0 0 0-.708z" />
+                <path fill-rule="evenodd" d="M10 8a.5.5 0 0 0-.5-.5H3a.5.5 0 0 0 0 1h6.5A.5.5 0 0 0 10 8zm2.5 6a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 1 0v11a.5.5 0 0 1-.5.5z" />
+            </svg>
+        </a>
 
 
-
-            <label class="description" for="element_7">Type de ressource </label>
-            <span>
-                <input id="element_7_2" name="element_7_2" oninput="search(this)" class="element checkbox" type="checkbox" value="1" />
-                <label class="choice" for="element_7_2">Fiche pédagogique- fp</label>
-                <input id="element_7_3" name="element_7_3" class="element checkbox" oninput="search(this)" type="checkbox" value="1" />
-                <label class="choice" for="element_7_3">Jeux - j</label>
-                <input oninput="search(this)" id="element_7_4" name="element_7_4" class="element checkbox" type="checkbox" value="1" />
-                <label class="choice" for="element_7_4">Doc authentique - da</label>
-                <input id="element_7_5" oninput="search(this)" name="element_7_5" class="element checkbox" type="checkbox" value="1" />
-                <label class="choice" for="element_7_5">video/site internet/mp3 - vsm</label>
-                <input id="element_7_6" oninput="search(this)" name="element_7_6" class="element checkbox" type="checkbox" value="1" />
-                <label class="choice" for="element_7_6">doc authentique - da</label>
-                <input id="element_7_7" oninput="search(this)" name="element_7_7" class="element checkbox" type="checkbox" value="1" />
-                <label class="choice" for="element_7_7">exos/activité - ea</label>
-            </span>
-
-            <hr>
-            <label class="description" for="element_5">Format de document </label>
-            <span>
-                <input id="element_5_1" oninput="search(this)" name="element_5_1" class="element checkbox" type="checkbox" value="1" />
-                <label class="choice" for="element_5_1">PDF</label>
-                <input id="element_5_2" name="element_5_2" oninput="search(this)" class="element checkbox" type="checkbox" value="1" />
-                <label class="choice" for="element_5_2">Image (jpeg/png/gif/..)</label>
-                <input id="element_5_3" name="element_5_3" class="element checkbox" oninput="search(this)" type="checkbox" value="1" />
-                <label class="choice" for="element_5_3">Audio (mp3/aac/..)</label>
-                <input oninput="search(this)" id="element_5_4" name="element_5_4" class="element checkbox" type="checkbox" value="1" />
-                <label class="choice" for="element_5_4">Video (mp4/mpeg,..)</label>
-                <input id="element_5_5" oninput="search(this)" name="element_5_5" class="element checkbox" type="checkbox" value="1" />
-                <label class="choice" for="element_5_5">Text (docx/doc/txt..)</label>
-                <input id="element_5_6" oninput="search(this)" name="element_5_6" class="element checkbox" type="checkbox" value="1" />
-                <label class="choice" for="element_5_6">Diaporama (pptx,..)</label>
-                <hr>
-                <label class="description" for="element_1">Recherche par etiquette</label>
-                <div>
-                    <input disabled list="format" id="element_1" name="element_1" onblur="search(this)" class="element text large" type="search" maxlength="255" value="" placeholder="&#128269; exemple : verbe+present" />
-                    <a data-tooltip="Séparez les étiquettes par des signes + si il y en a plusieurs" style="padding: 0;border: none;background: none; margin: 49%;">
-                        <svg class="bi bi-question-circle" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                            <path d="M5.25 6.033h1.32c0-.781.458-1.384 1.36-1.384.685 0 1.313.343 1.313 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.007.463h1.307v-.355c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.326 0-2.786.647-2.754 2.533zm1.562 5.516c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z" />
-                        </svg>
-                    </a>
-
+        <div id="sortpanel" style="display: block;">
+            <form>
+                <label class="description" for="element_1">Rechercher par nom </label>
+                <div style="float: left;">
+                    <input onblur="search(this)" id="element_1" name="docname" class="element text large" type="search" maxlength="255" value="" placeholder="Recherche par nom" />
+                    <hr>
                 </div>
-            </span>
-            <hr>
-            <label class="description" for="element_3">Niveau </label>
-            <span>
-                <input id="element_3_1" name="element_3_1" class="element checkbox" type="checkbox" oninput="search(this)" value="1" />
-                <label class="choice" for="element_3_1">ALPHA</label>
-                <input id="element_3_2" name="element_3_2" class="element checkbox" type="checkbox" oninput="search(this)" value="1" />
-                <label class="choice" for="element_3_2">D</label>
-                <input id="element_3_3" name="element_3_3" class="element checkbox" type="checkbox" oninput="search(this)" value="1" />
-                <label class="choice" for="element_3_3">E</label>
-                <input id="element_3_4" name="element_3_4" class="element checkbox" type="checkbox" oninput="search(this)" value="1" />
-                <label class="choice" for="element_3_4">A</label>
-            </span>
-            <hr>
-            <label class="description" for="element_4">Trier par </label>
-            <span>
-                <input id="element_4_1" name="element_4" class="element radio" type="radio" oninput="search(this)" value="1" />
-                <label class="choice" for="element_4_1">Alphabétique croissant</label>
-                <input id="element_4_2" name="element_4" class="element radio" type="radio" oninput="search(this)" value="2" />
-                <label class="choice" for="element_4_2">Alphabétique décroissant</label>
-                <input disabled id="element_4_3" name="element_4" class="element radio" type="radio" oninput="search(this)" value="1" />
-                <label class="choice" for="element_4_3">Popularité croissant</label>
-                <input disabled id="element_4_4" name="element_4" class="element radio" type="radio" oninput="search(this)" value="2" />
-                <label class="choice" for="element_4_4">Popularité décroissant</label>
 
-            </span>
-            <hr>
-            <label class="description" for="element_6">Option </label>
-            <span>
-                <input disabled id="element_6_1" name="element_6_1" class="element checkbox" type="checkbox" oninput="search(this)" value="1" />
-                <label class="choice" for="element_6_1">TEF/ANF</label>
-            </span>
-            <hr>
-            <input style="margin: 1%;width: 98%;" type="reset" value="Vider le formulaire">
-        </form>
+
+
+                <label class="description" for="element_7">Type de ressource </label>
+                <span>
+                    <input id="element_7_2" name="element_7_2" oninput="search(this)" class="element checkbox" type="checkbox" value="1" />
+                    <label class="choice" for="element_7_2">Fiche pédagogique- fp</label>
+                    <input id="element_7_3" name="element_7_3" class="element checkbox" oninput="search(this)" type="checkbox" value="1" />
+                    <label class="choice" for="element_7_3">Jeux - j</label>
+                    <input oninput="search(this)" id="element_7_4" name="element_7_4" class="element checkbox" type="checkbox" value="1" />
+                    <label class="choice" for="element_7_4">Doc authentique - da</label>
+                    <input id="element_7_5" oninput="search(this)" name="element_7_5" class="element checkbox" type="checkbox" value="1" />
+                    <label class="choice" for="element_7_5">video/site internet/mp3 - vsm</label>
+                    <input id="element_7_6" oninput="search(this)" name="element_7_6" class="element checkbox" type="checkbox" value="1" />
+                    <label class="choice" for="element_7_6">doc authentique - da</label>
+                    <input id="element_7_7" oninput="search(this)" name="element_7_7" class="element checkbox" type="checkbox" value="1" />
+                    <label class="choice" for="element_7_7">exos/activité - ea</label>
+                </span>
+
+                <hr>
+                <label class="description" for="element_5">Format de document </label>
+                <span>
+                    <input id="element_5_1" oninput="search(this)" name="element_5_1" class="element checkbox" type="checkbox" value="1" />
+                    <label class="choice" for="element_5_1">PDF</label>
+                    <input id="element_5_2" name="element_5_2" oninput="search(this)" class="element checkbox" type="checkbox" value="1" />
+                    <label class="choice" for="element_5_2">Image (jpeg/png/gif/..)</label>
+                    <input id="element_5_3" name="element_5_3" class="element checkbox" oninput="search(this)" type="checkbox" value="1" />
+                    <label class="choice" for="element_5_3">Audio (mp3/aac/..)</label>
+                    <input oninput="search(this)" id="element_5_4" name="element_5_4" class="element checkbox" type="checkbox" value="1" />
+                    <label class="choice" for="element_5_4">Video (mp4/mpeg,..)</label>
+                    <input id="element_5_5" oninput="search(this)" name="element_5_5" class="element checkbox" type="checkbox" value="1" />
+                    <label class="choice" for="element_5_5">Text (docx/doc/txt..)</label>
+                    <input id="element_5_6" oninput="search(this)" name="element_5_6" class="element checkbox" type="checkbox" value="1" />
+                    <label class="choice" for="element_5_6">Diaporama (pptx,..)</label>
+                    <hr>
+                    <label class="description" for="element_1">Recherche par etiquette</label>
+                    <div>
+                        <input disabled list="format" id="element_1" name="element_1" onblur="search(this)" class="element text large" type="search" maxlength="255" value="" placeholder="&#128269; exemple : verbe+present" />
+                        <a data-tooltip="Séparez les étiquettes par des signes + si il y en a plusieurs" style="padding: 0;border: none;background: none; margin: 49%;">
+                            <svg class="bi bi-question-circle" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                <path d="M5.25 6.033h1.32c0-.781.458-1.384 1.36-1.384.685 0 1.313.343 1.313 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.007.463h1.307v-.355c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.326 0-2.786.647-2.754 2.533zm1.562 5.516c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z" />
+                            </svg>
+                        </a>
+
+                    </div>
+                </span>
+                <hr>
+                <label class="description" for="element_3">Niveau </label>
+                <span>
+                    <input id="element_3_1" name="element_3_1" class="element checkbox" type="checkbox" oninput="search(this)" value="1" />
+                    <label class="choice" for="element_3_1">ALPHA</label>
+                    <input id="element_3_2" name="element_3_2" class="element checkbox" type="checkbox" oninput="search(this)" value="1" />
+                    <label class="choice" for="element_3_2">D</label>
+                    <input id="element_3_3" name="element_3_3" class="element checkbox" type="checkbox" oninput="search(this)" value="1" />
+                    <label class="choice" for="element_3_3">E</label>
+                    <input id="element_3_4" name="element_3_4" class="element checkbox" type="checkbox" oninput="search(this)" value="1" />
+                    <label class="choice" for="element_3_4">A</label>
+                </span>
+                <hr>
+                <label class="description" for="element_4">Trier par </label>
+                <span>
+                    <input id="element_4_1" name="element_4" class="element radio" type="radio" oninput="search(this)" value="1" />
+                    <label class="choice" for="element_4_1">Alphabétique croissant</label>
+                    <input id="element_4_2" name="element_4" class="element radio" type="radio" oninput="search(this)" value="2" />
+                    <label class="choice" for="element_4_2">Alphabétique décroissant</label>
+                    <input disabled id="element_4_3" name="element_4" class="element radio" type="radio" oninput="search(this)" value="1" />
+                    <label class="choice" for="element_4_3">Popularité croissant</label>
+                    <input disabled id="element_4_4" name="element_4" class="element radio" type="radio" oninput="search(this)" value="2" />
+                    <label class="choice" for="element_4_4">Popularité décroissant</label>
+
+                </span>
+                <hr>
+                <label class="description" for="element_6">Option </label>
+                <span>
+                    <input disabled id="element_6_1" name="element_6_1" class="element checkbox" type="checkbox" oninput="search(this)" value="1" />
+                    <label class="choice" for="element_6_1">TEF/ANF</label>
+                </span>
+                <hr>
+                <input style="margin: 1%;width: 98%;" type="reset" value="Vider le formulaire">
+            </form>
+        </div>
     </div>
 
     <main class="main">
