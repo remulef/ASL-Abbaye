2d8cacdc254df55a958062124b517927d6934b76
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-06-24 12:30:25 +0200

    automatic push

--- View/display.view/display.js
+++ View/display.view/display.js
@@ -168,7 +168,7 @@ function search(elem) {
 
     console.log(param);
     try {
-        let url = "http://www.les-asl-ab3baye.ovh/ASL-Abbaye/Controler/Display/script-search.php";
+        //let url = "http://www.les-asl-ab3baye.ovh/ASL-Abbaye/Controler/Display/script-search.php";
         let url = "http://www.les-asl-ab3baye.ovh/ASL-Abbaye/Controler/Display/test.php";
         //ajax_post_request(displaysearch, url, true, encodeURIComponent(param));
         ajax_post_request(log, url, true, encodeURIComponent(param));
