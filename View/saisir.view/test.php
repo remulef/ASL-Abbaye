95f3c8c1f7ca0a4e67ad348dfa4db7f12db88f66
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-06-26 17:10:44 +0200

    automatic push

--- View/saisir.view/test.php
+++ View/saisir.view/test.php
@@ -3,7 +3,7 @@
 
 
 // Include the main TCPDF library (search for installation path).
-require_once('TCPDF-master/exemple/tcpdf_include.php');
+require_once('TCPDF-master/tcpdf.php');
 
 // create new PDF document
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
