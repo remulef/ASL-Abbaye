09747842f48dd5b3080e6af3b1aa129bbb0b68d9
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-06-26 23:19:57 +0200

    automatic push

--- View/saisir.view/test.php
+++ View/saisir.view/test.php
@@ -44,10 +44,46 @@ $pdf->writeHTML($html, true, false, true, false, '');
 $pdf->lastPage();
 //Close and output PDF document
 $pdf->Output($_POST['title'], 'I');
+
+
+$acc_type = array('gif', 'jpg', 'jpe', 'jpeg', 'png', 'pdf', 'docx', 'doc', 'ppx', 'pptx', 'mp3', 'aac', 'txt', 'odt', 'mp4', 'odt');
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 */
 
+// Vérifier si le formulaire a été soumis
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Vérifie si le fichier a été uploadé sans erreur.
+    if (isset($_FILES["fileToUpload"]) && $_FILES["fileToUpload"]["error"] == 0) {
+        $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
+        $filename = $_FILES["fileToUpload"]["name"];
+        $filetype = $_FILES["fileToUpload"]["type"];
+        $filesize = $_FILES["fileToUpload"]["size"];
 
-var_dump($_FILES['files']);
+        // Vérifie l'extension du fichier
+        $ext = pathinfo($filename, PATHINFO_EXTENSION);
+        if (!array_key_exists($ext, $allowed)) die("Erreur : Veuillez sélectionner un format de fichier valide.");
 
+        // Vérifie la taille du fichier - 5Mo maximum
+        $maxsize = 5 * 1024 * 1024;
+        if ($filesize > $maxsize) die("Error: La taille du fichier est supérieure à la limite autorisée.");
 
-?>
+        // Vérifie le type MIME du fichier
+        if (in_array($filetype, $allowed)) {
+            // Vérifie si le fichier existe avant de le télécharger.
+            if (file_exists("upload/" . $_FILES["fileToUpload"]["name"])) {
+                echo $_FILES["fileToUpload"]["name"] . " existe déjà.";
+            } else {
+                move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], "upload/" . $_FILES["fileToUpload"]["name"]);
+                echo "Votre fichier a été téléchargé avec succès.";
+            }
+        } else {
+            echo "Error: Il y a eu un problème de téléchargement de votre fichier. Veuillez réessayer.";
+        }
+    } else {
+        echo "Error: " . $_FILES["fileToUpload"]["error"];
+    }
+}
+
