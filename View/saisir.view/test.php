5f2f30e61366a26a6183aa178d4c2302cfb4205c
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-06-27 19:12:21 +0200

    automatic push

--- View/saisir.view/test.php
+++ View/saisir.view/test.php
@@ -43,7 +43,7 @@ $pdf->writeHTML($html, true, false, true, false, '');
 // reset pointer to the last page
 $pdf->lastPage();
 //Close and output PDF document
-$pdf->Output("../../../tmp-CR/".$_POST["titre"], 'F');
+$pdf->Output("../../../tmp-CR/".$_POST["titre"]."pdf", 'F');
 
 
 
