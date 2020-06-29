f959b8125a1a21420a69052b0a5bff588c8213fc
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-06-29 23:30:04 +0200

    automatic push

---
+++ View/proposer.view/proposer.view.php
@@ -0,0 +1,17 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proposer</title>
+</head>
+<body>
+    <form action="" method="post" enctype="multipart/form-data">
+    <textarea name="" id="" cols="30" rows="10">Rapide description du document que vous proposer</textarea>
+    <input type="file" name="" id="">
+    <input type="text" name="" id="" place>
+    
+    </form>
+</body>
+</html>
