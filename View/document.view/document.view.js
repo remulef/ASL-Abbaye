c853d26846b7adcdc2c1415802ee5dd9fa4cd75b
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-06-29 17:56:09 +0200

    automatic push

--- View/document.view/document.view.js
+++ View/document.view/document.view.js
@@ -763,7 +763,7 @@ function recuptag(json) {
 function AJAXgettag() {
  try{
     let url = "http://www.les-asl-abbaye.ovh/ASL-Abbaye/Controler/document/recup-tag.php?";
-    ajax_post_request(recuptag, url, false, encodeURIComponent(id_doc));
+    ajax_post_request(AJAXgettag, url, false, encodeURIComponent(id_doc));
 }  catch (error) {
     alert(error);
 }
