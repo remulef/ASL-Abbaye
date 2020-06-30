60be1179cfa0c20b790998c8a815445126662fa9
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-06-30 23:27:50 +0200

    automatic push

--- View/saisir.view/form.php
+++ View/saisir.view/form.php
@@ -6,6 +6,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Saisir un compte-rendu</title>
 	<link rel="stylesheet" type="text/css" href="view.css" media="all">
+	<script src="ckeditor/ckeditor.js"></script>
 	<script type="text/javascript" src="view.js"></script>
 	<script type="text/javascript" src="calendar.js"></script>
 </head>
