b99ffd58a9355faa64d2e11a7474c7dc31f72fa6
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-07-02 23:40:18 +0200

    automatic push

--- View/display.view/display.php
+++ View/display.view/display.php
@@ -51,22 +51,18 @@
             <form>
 
                 <label class="description" for="element_8">Lieu de recherche </label>
-                    <div class="switch-box">
-                        <label for="default" class="label-l">BDD</label>
+                <input type="checkbox" id="id-name--1" name="set-name" class="switch-input">
 
-                        <input id="default" class="switch-box-input" type="checkbox" oninput="careful()" />
-                        <label for="default" class="switch-box-slider"></label>
+                <label for="id-name--1" class="switch-label">Recherche dans
+                    <span class="toggle--on">BDD</span>
+                    <span class="toggle--off">Dossier courant</span></label>
 
-                        <label for="default" class="label-r">Dossier courant</label>
-
-                        <a data-tooltip="La recherche sera effectuer soit dans l'entiereté des documents disponible soit dans le dossier ou vous vous situez actuellement" style="padding: 0;border: none;background: none; margin: 49%;">
-                            <svg class="bi bi-question-circle" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                <path d="M5.25 6.033h1.32c0-.781.458-1.384 1.36-1.384.685 0 1.313.343 1.313 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.007.463h1.307v-.355c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.326 0-2.786.647-2.754 2.533zm1.562 5.516c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z" />
-                            </svg>
-                        </a>
-
-                    </div>
+                <a data-tooltip="La recherche sera effectuer soit dans l'entiereté des documents disponible soit dans le dossier ou vous vous situez actuellement" style="padding: 0;border: none;background: none; margin: 49%;">
+                    <svg class="bi bi-question-circle" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                        <path d="M5.25 6.033h1.32c0-.781.458-1.384 1.36-1.384.685 0 1.313.343 1.313 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.007.463h1.307v-.355c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.326 0-2.786.647-2.754 2.533zm1.562 5.516c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z" />
+                    </svg>
+                </a>
                 <hr>
 
                 <label class="description" for="element_1">Rechercher par nom </label>
@@ -111,7 +107,7 @@
                     <hr>
                     <label class="description" for="element_1">Recherche par etiquette (desactivé)</label>
                     <div>
-                        <input  list="format" id="element_1" name="element_1" onblur="search(this)" class="element text large" type="search" maxlength="255" value="" placeholder="&#128269; exemple : verbe+present" />
+                        <input list="format" id="element_1" name="element_1" onblur="search(this)" class="element text large" type="search" maxlength="255" value="" placeholder="&#128269; exemple : verbe+present" />
                         <a data-tooltip="Séparez les étiquettes par des signes + si il y en a plusieurs" style="padding: 0;border: none;background: none; margin: 49%;">
                             <svg class="bi bi-question-circle" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
@@ -154,7 +150,7 @@
                     <label class="choice" for="element_6_1">TEF/ANF</label>
                     <input disabled id="element_6_2" name="element_6_2" class="element checkbox" type="checkbox" oninput="search(this)" value="1" />
                     <label class="choice" for="element_6_2">Niveau ALPHA</label>
-                
+
                 </span>
                 <hr>
                 <input style="margin: 1%;width: 98%;" type="reset" value="Vider le formulaire">
