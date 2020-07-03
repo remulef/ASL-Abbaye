ca4d55a35b0e1f85a268c6717da7f65625894871
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-07-03 12:45:01 +0200

    automatic push

--- View/document.view/mitigeur.php
+++ View/document.view/mitigeur.php
@@ -2,7 +2,7 @@
 session_start();
 $id_doc = $_GET["id_doc"];
 
-if(isset($_SESSION['role']) && ($_SESSION['role'][0] == "ADMINISTRATEUR" || $_SESSION['role'][0] == "MODERATEUR" 
+if(isset($_SESSION['role']) && ($_SESSION['role'][0] == "administrateur" || $_SESSION['role'][0] == "MODERATEUR" 
 || $_SESSION['role'][0] == "BENEVOLE ABBAYE")){
     $url = "Location: "."http://les-asl-abbaye.ovh/ASL-Abbaye/View/document.view.admin.php?id_doc=".$id_doc;
     header($url);
