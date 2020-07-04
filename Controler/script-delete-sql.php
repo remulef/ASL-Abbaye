086ec9cd94e179d8065cf49eec69e6badafeced9
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-07-04 22:24:12 +0200

    automatic push

--- Controler/script-delete-sql.php
+++ Controler/script-delete-sql.php
@@ -21,15 +21,10 @@ if (true) {
     die('Erreur : ' . $e->getMessage());
   }
 
-  $sql = 'SELECT tmp FROM DOCUMENT WHERE id_doc = ' . $id_doc;
-  $tmp = $db->query($sql)->fetchColumn();
+  $sql = 'SELECT chemin FROM DOCUMENT WHERE id_doc = ' . $id_doc;
+  $chemin = $db->query($sql)->fetchColumn();
 
 
-
-  if ($tmp == false) {
-echo "lol";
-  }
-  
   $sth = $db->prepare('DELETE  FROM NODE_DOCUMENT WHERE DOCUMENT_id_doc = ?');
   $sth->bindParam(1, $id_doc);
   $sth->execute();
@@ -42,12 +37,19 @@ echo "lol";
   $sth->bindParam(1, $id_doc);
   $sth->execute();
 
-
   $sth = $db->prepare('DELETE  FROM DOCUMENT WHERE id_doc = ?');
   $sth->bindParam(1, $id_doc);
+  $sth->execute();
 
-  echo $sth->execute();
-  //$res = $sth->fetch();
+  if ($sth->execute()) {
+
+    try {
+      echo $_SERVER['DOCUMENT_ROOT'].$chemin;
+      unlink($_SERVER['DOCUMENT_ROOT'].$chemin);
+    } catch (Throwable $th) {
+      throw $th;
+    }
+  };
 
   $db = null;
 }
