c3faf8fddbe73f50af7bd51b9a4de90607f59d75
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-07-04 17:07:55 +0200

    automatic push

--- View/document.view/mitigeur.php
+++ View/document.view/mitigeur.php
@@ -8,20 +8,17 @@ var_dump($_SESSION);
 file_put_contents("dump.txt", ob_get_flush());
 
 $fp = fopen('log.txt', 'a+');
-fwrite($fp, $_SESSION['role'][0]);
-fwrite($fp,(isset($_SESSION['role']) && in_array( $_SESSION['role'][0],$allowed)));
-
 
 
 if(isset($_SESSION['role']) && in_array( $_SESSION['role'][0],$allowed)){
     
     $url = "Location: "."http://les-asl-abbaye.ovh/ASL-Abbaye/View/document.view/document.view.admin.php?id_doc=".$id_doc;
-    fwrite($fp,"admin");
+    fwrite($fp,"admin".PHP_EOL);
 
     header($url);
 }else {
     $url = "Location: "."http://les-asl-abbaye.ovh/ASL-Abbaye/View/document.view/document.view.php?id_doc=".$id_doc;
-    fwrite($fp,"pas admin");
+    fwrite($fp,"pas admin".PHP_EOL);
 
     header($url);
 }
