09bdb222259ce06d6a4ee4f72d1b0999356552fc
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-07-06 13:32:00 +0200

    automatic push

--- View/admin.view/gestiondoc.php
+++ View/admin.view/gestiondoc.php
@@ -56,20 +56,6 @@ var_dump($_SESSION);
 
 
       ?>
-
-      <li>
-      <h3>006synth�se</h3>
-        <p class="price">pdf</p>
-        <a href="http://les-asl-abbaye.ovh/ASL-Abbaye/View/document.view/document.view.admin.php?id_doc=2035">
-          Ouvrir</a>
-      </li>
-      <li>
-        <img src="http://placehold.it/200x120">
-        <h3>005u</h3>
-        <p class="price">pdf </p>
-        <a href="http://les-asl-abbaye.ovh/ASL-Abbaye/View/document.view/document.view.admin.php?id_doc=2036">
-          Ouvrir</a>
-      </li>
       </ul>
     <?php
   } else {
