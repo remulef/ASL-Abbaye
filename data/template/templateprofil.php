1c45becd67d2a305631ae80dd983f10a581ab995
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-07-07 17:25:38 +0200

    automatic push

--- data/template/templateprofil.php
+++ data/template/templateprofil.php
@@ -2,9 +2,24 @@
 session_start();
 echo 
 ' <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script> if ( $.browser.msie ) {
-  alert( $.browser.version );
-} </script>';
+<script>
+function msieversion() {
+
+  var ua = window.navigator.userAgent;
+  var msie = ua.indexOf("MSIE ");
+
+  if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
+  {
+      alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
+  }
+  else  // If another browser, return 0
+  {
+      alert("otherbrowser");
+  }
+
+  return false;
+}
+</script>';
   if(isset($_SESSION['role']) && $_SESSION['role'] != ""){
     $role = $_SESSION['role'];
     if($role[0] == "BENEVOLE ABBAYE"){
