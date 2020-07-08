a033721ac0c0d8200b67cb6637cc9d8354b35624
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-07-08 21:21:36 +0200

    automatic push

--- View/document.view/document.view.php
+++ View/document.view/document.view.php
@@ -122,14 +122,21 @@ echo '<body onload="init(' . $id_doc . ')">';
             <form id="ss-form" onsubmit="submitted=true" action="https://docs.google.com/forms/d/1-PRLoHTtgldV5cNTvmVyjf-rf1p1kLhzKGXn2i1XGhQ/formResponse" method="POST" target="hidden_iframe">
                 <input name="Nom" class="form-control" type="text" value="" id="entry_name" placeholder="Votre nom">
                 <input name="entry.1732478127" title="" class="ss-q-short form-control" id="entry_comment" dir="auto" aria-label="Message  " type="text" value="" placeholder="Votre commentaire">
-                
+
                 <input name="draftResponse" type="hidden" value='[,,"-2195881827543510175"]&#10;'>
                 <input name="pageHistory" type="hidden" value="0">
                 <input name="fvv" type="hidden" value="0">
                 <input name="fbzx" type="hidden" value="-2195881827543510175">
-
-                <input name="submit" class="jfk-button jfk-button-action btn btn-primary" id="ss-submit" type="submit" value="Commenter" onClick="return check()">
-                <input name="reset" class="jfk-button jfk-button-action btn btn-primary" id="ss-reset" type="reset" value="réinitialiser" onClick="">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                        <input name="submit" class="jfk-button jfk-button-action btn btn-primary" id="ss-submit" type="submit" value="Commenter" onClick="return check()">
+                        </div>
+                        <div class="col-md-4 offset-md-4">
+                        <input name="reset" class="jfk-button jfk-button-action btn btn-primary" id="ss-reset" type="reset" value="Réinitialiser" onClick="">
+                        </div>
+                    </div>
+                </div>
             </form>
 
         </div>
