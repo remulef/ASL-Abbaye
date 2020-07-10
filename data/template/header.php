d4543694eb5963287fdd588b0d7c7ed974cc9518
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-07-10 14:09:52 +0200

    automatic push

--- data/template/header.php
+++ data/template/header.php
@@ -20,29 +20,29 @@ if(isset($_SESSION)){
   <?php if (isset($_SESSION['role']) && $_SESSION['role'] != "") {
     echo '
     <div class="header-user-menu">
-    <img src="assets/2.jpg" alt="User Image"/>
+    <img src="logoasl.png" alt="User Image"/>
       <ul>';
     if ($role[0] == "ADMINISTRATEUR") {
       echo '<li><p class="highlight" >Mon rôle :   ADMINISTRATEUR </p></li>
         <li><a href="http://les-asl-abbaye.ovh/ASL-Abbaye/View/admin.view/user-management.php">Gestion des comptes</a></li>
         <li><a href="http://les-asl-abbaye.ovh/ASL-Abbaye/View/admin.view/gestiondoc.php">Gestion des documents</a></li>
         <li><a href="http://les-asl-abbaye.ovh/ASL-Abbaye/View/accueil.view/main\'.php?deconnexion=true">Déconnexion</a></li>
-        </ul>';
+        </ul>  </div>';
     } else {
       echo sprintf('
         <li><p>Mon rôle : %s </p><li>
         <li><a href="main\'.php?deconnexion=true">Déconnexion</a><li>
-        </ul>', $role[0]);
+        </ul>  </div>', $role[0]);
     }
   } else {
     echo '<ul>
-    <a href="http://les-asl-abbaye.ovh/ASL-Abbaye/View/authentication.view/login.php\">
+    <a href="http://les-asl-abbaye.ovh/ASL-Abbaye/View/authentication.view/login.php">
     Connexion  
     </a>
     <ul>';
   }
   ?>
 
-  </div>
+
 
 </header>
