7afd90bbfe87480df474861acde4282ae964a50b
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-07-10 15:33:10 +0200

    automatic push

--- View/proposer.view/proposer.php
+++ View/proposer.view/proposer.php
@@ -74,8 +74,8 @@
 				-->
 					<li class="buttons">
 						<input type="hidden" name="form_id" value="116196" />
-						<input type="reset" class="btn btn-primary" value="Reinitialiser">
-						<input id="saveForm" class="button_text btn btn-primary" type="submit" name="submit" value="Proposer" />
+						<input type="reset" class="button_text btn btn-primary" value="Reinitialiser">
+						<input id="saveForm" class=" btn btn-primary" type="submit" name="submit" value="Proposer" />
 					</li>
 				</ul>
 			</form>
