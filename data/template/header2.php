2eb25f7e34da71e480812220d8f981d981a02879
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-07-11 00:04:28 +0200

    automatic push

--- data/template/header2.php
+++ data/template/header2.php
@@ -3,9 +3,11 @@ var_dump($_POST);
 var_dump($_SESSION);
 
 $do = true;
-if ( $_SERVER["REQUEST_METHOD"] == "POST" && $_POST["deconnexion"] == "Deconnexion" && isset($_SESSION)) {
+if ( $_SERVER["REQUEST_METHOD"] == "POST" && $_POST["deconnexion"] == "Deconnexion" && isset($_SESSION["role"])) {
     echo "vide le Session";
-    session_unset();
+    session_unset($_SESSION["role"]);
+    session_unset($_SESSION["password"]);
+    session_unset($_SESSION["username"]);
     $do = false;
 }
 
