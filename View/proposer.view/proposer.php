30eca444eda4dc1c635a7a0f7ccea6b57b2b5c96
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-07-10 15:11:06 +0200

    automatic push

--- View/proposer.view/proposer.php
+++ View/proposer.view/proposer.php
@@ -9,7 +9,7 @@
 	<title>Proposer un document</title>
 <link rel="stylesheet" type="text/css" media="screen" href="http://les-asl-abbaye.ovh/ASL-Abbaye/data/template/footer.css" />
 	
-	<link rel="stylesheet" type="text/css" href="view2.css" media="all">
+	<link rel="stylesheet" type="text/css" href="view.css" media="all">
 	<link rel="stylesheet" type="text/css" media="screen" href="http://les-asl-abbaye.ovh/ASL-Abbaye/data/template/template.css" />
 	<link rel="stylesheet" type="text/css" media="screen" href="http://les-asl-abbaye.ovh/ASL-Abbaye/data/template/header.css" />
 
