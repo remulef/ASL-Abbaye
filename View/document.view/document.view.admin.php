43546eb2256b1fa4b792a9b6013d84abbc55107b
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-07-13 22:53:23 +0200

    automatic push

--- View/document.view/document.view.admin.php
+++ View/document.view/document.view.admin.php
@@ -17,9 +17,9 @@ if (!isset($_SESSION['role'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://les-asl-abbaye.ovh/ASL-Abbaye/data/template/template.css" />
-    <link rel="stylesheet" type="text/css" media="screen" href="http://les-asl-abbaye.ovh/ASL-Abbaye/data/template/footer.css" />
     <link rel="stylesheet" type="text/css" href="document.view.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="http://les-asl-abbaye.ovh/ASL-Abbaye/data/template/header.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="http://les-asl-abbaye.ovh/ASL-Abbaye/data/template/footer.css" />
     <link rel="stylesheet" type="text/css" media="screen" href="bootstrap-forms.css" />
 
     <title>Document ASL Abbaye</title>
