7b2cb1049d1a71764f9cdb86cd28e5a5ab461557
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-07-13 21:37:20 +0200

    automatic push

--- View/contact.view/contact.php
+++ View/contact.view/contact.php
@@ -14,17 +14,46 @@
     <?php
       include ("{$_SERVER['DOCUMENT_ROOT']}/ASL-Abbaye/data/template/header.php");
     ?>
-    <h1 class="az">Nous contacter</h1>
-    <form action="mailto:asl.abbaye@grenoble.fr" method="post" enctype="text/plain">
 
-      <p id="J">Votre Message : </p>
-      <textarea required  class="form-control"  type="text" name="Message" value="">
-      </textarea>
-      <input type="submit" class="btn btn-primary" name="" value="Envoyer">
-      <input type="reset" class="btn btn-primary" name="" value="Réinitialiser">
-      <p>Ce message sera envoyé à :</p>
-      <p id="Ju">asl.abbaye@grenoble.fr</p>
-    </form>
+<div class="container">
+	<div class="row">
+			<h1>Nous contacter</h1>
+	</div>
+	<div class="row">
+			<h4 style="text-align:center">Nous aimerions avoir de vos nouvelles </h4>
+	</div>
+	<div class="row input-container">
+			<div class="col-xs-12">
+				<div class="styled-input wide">
+					<input type="text" required />
+					<label>Nom</label> 
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-12">
+				<div class="styled-input">
+					<input type="text" required />
+					<label>Email</label> 
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-12">
+				<div class="styled-input" style="float:right;">
+					<input type="text" required />
+					<label>Numero de téléphone</label> 
+				</div>
+			</div>
+			<div class="col-xs-12">
+				<div class="styled-input wide">
+					<textarea required></textarea>
+					<label>Message</label>
+				</div>
+			</div>
+			<div class="col-xs-12">
+				<div class="btn-lrg submit-btn">Envoyer le message</div>
+			</div>
+	</div>
+</div>
+
+
     <?php include("{$_SERVER['DOCUMENT_ROOT']}/ASL-Abbaye/data/template/footer_template.php"); ?>
 
   </body>
