c05469960b5846f807198dd55599979d2c16a735
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-07-16 21:37:07 +0200

    automatic push

--- View/display.view/display.php
+++ View/display.view/display.php
@@ -72,10 +72,10 @@
 
 
                         <label class="description" for="element_1">Rechercher par nom </label>
-                        <div >
+                        <span>
                             <input onblur="search(this)" id="element_1" name="docname" class="element text large form-control" type="search" maxlength="255" value="" placeholder="Recherche par nom" />
 
-                        </div>
+                        </span>
 
                     
 
