4cb007957e8c45052f4ba8781f46c18f353742ba
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-07-16 20:48:32 +0200

    automatic push

--- View/display.view/display.js
+++ View/display.view/display.js
@@ -447,14 +447,14 @@ function hidepan() {
         hider.innerHTML = "afficher le panel <br> de tri/recherche <br> <svg class=\"bi bi-arrow-bar-right\" width=\"1.33em\" height=\"1.33em\" viewBox=\"0 0 16 16\" fill=\"currentColor\" xmlns=\"http://www.w3.org/2000/svg\">"
             + "<path fill-rule=\"evenodd\" d=\"M10.146 4.646a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L12.793 8l-2.647-2.646a.5.5 0 0 1 0-.708z\"/>" +
             "<path fill-rule=\"evenodd\" d=\"M6 8a.5.5 0 0 1 .5-.5H13a.5.5 0 0 1 0 1H6.5A.5.5 0 0 1 6 8zm-2.5 6a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 1 0v11a.5.5 0 0 1-.5.5z\"/></svg>"
-            document.getElementById("content").setAttribute("class","content col-sm-9");
+            document.getElementById("content").setAttribute("class","content col-sm-12");
         } else {
         sortpanel.setAttribute("style", "display: block;");
         hider.innerHTML =
             "cacher le panel <br> de tri/recherche <br> <svg class=\"bi bi-arrow-bar-left\" width=\"1.33em\" height=\"1.33em\" viewBox=\"0 0 16 16\" fill=\"currentColor\" xmlns=\"http://www.w3.org/2000/svg\">" +
             "<path fill-rule=\"evenodd\" d=\"M5.854 4.646a.5.5 0 0 0-.708 0l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L3.207 8l2.647-2.646a.5.5 0 0 0 0-.708z\" />" +
             "<path fill-rule=\"evenodd\" d=\"M10 8a.5.5 0 0 0-.5-.5H3a.5.5 0 0 0 0 1h6.5A.5.5 0 0 0 10 8zm2.5 6a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 1 0v11a.5.5 0 0 1-.5.5z\" /></svg>";
-        document.getElementById("content").setAttribute("class","content col-sm-12");
+        document.getElementById("content").setAttribute("class","content col-sm-9");
     };
 }
 
