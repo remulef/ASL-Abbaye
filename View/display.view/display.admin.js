c8abe881e890e4a2ab5be04ae96c51c6e811f00d
Fabien Remule <50622976+remulef@users.noreply.github.com>
2020-11-14 02:19:33 +0100

    automatic push

--- View/display.view/display.admin.js
+++ View/display.view/display.admin.js
@@ -29,7 +29,7 @@ function load(json) {
     var uldoss = document.getElementById("dossbar");
     for (let index = 0; index < data.length; index++) {
         var current = data[index];
-        if (current.id_node !== undefined && current.name!=="Réalisation" && current.name!=="Comptes-rendus") {
+        if (current.id_node !== undefined) {
             var id = current.id_node;
             var li = document.createElement("li");
             var a = document.createElement("a");
--- View/display.view/display.php
+++ View/display.view/display.php
@@ -189,7 +189,11 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 
 <!-- Optional JavaScript -->
-<script src="display.js"></script>
+<?php
+
+$allowed = array("ADMINISTRATEUR", "MODERATEUR", "BENEVOLE ABBAYE");
+
+(!in_array($_SESSION['role'], $allowed)?echo 'PAS ADMIN <script src="display.js"></script>': echo' ADMIN <script src="display.admin.js"></script>') ?>
 <script>
     $(document).keypress(function(e) {
         if (e.which == 13) {
